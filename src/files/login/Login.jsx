f6a19e8e0e59d2ad76013ea10520c7523b8de1ef
Tanya <vermatanya932@gmail.com>
2024-04-06 00:32:10 +0530

    added api

--- src/files/login/Login.jsx
+++ src/files/login/Login.jsx
@@ -1,115 +1,142 @@
-import React from 'react';
-import {Link,useHistory} from 'react-dom'
-import useState from 'react'
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Login() {
-    const [inputs,setInputs] = useState({
-      username:"",
-      password:"",
-      // email:""
-    });
+  const navigate = useNavigate();
+  const [inputs, setInputs] = React.useState({
+    password: "",
+    email: "",
+  });
 
-    const handleChange = (e) =>{
-      setInputs((previousState) => ({
-        ...previousState,
-        [e.target.name]: e.target.value,
-      }));
-    };
-
-    const sendRequest = async () => {
-      // add your api endpoint for login 
-      console.log(inputs)
-      try
-       { const response = await axios
-       .post(`https://localhost/api/users/login`,{
-        username:inputs.username,
-        password:inputs.password,
-       })}
-       
-       catch(error) {
-        console.error(error);
-       }
-
-    };
-    const [password,setPassword] = useState('');
-    const history= useHistory();
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      console.log(inputs);
-      sendRequest()
-    // api calling
-    // try {
-    //   const response = await fetch('/api/signin', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ username, password }),
-    //   });
-
-    //   if(response.ok){
-    //     history.push('/home')
-    //   }
-    //   else
-    //   {
-    //     console.log('Login failed invalid username or password');
-    //   }
-    // } 
-    // catch(error) {
-    //   console.error('Error :',error)
-    // }
+  const handleChange = (e) => {
+    setInputs((previousState) => ({
+      ...previousState,
+      [e.target.name]: e.target.value,
+    }));
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/home");
+
+    // axios
+    //   .post(API_URL_REGISTER, inputs)
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+  };
+
   return (
-    
-    <div className="bg-black h-screen w-screen">
-    <div className="flex justify-center items-center h-screen ">
-      <div className="max-w-md w-full px-6 py-8">
-        <div className="text-center mb-8">
-          <h1 className="text-4xl font-bold text-white">Emotunes</h1>
-        </div>
-        <div className="bg-gray-800 rounded-lg shadow-lg p-8">
-          <h2 className="text-2xl mb-4 font-semibold text-white text-center">Sign In</h2>
-          <form className="space-y-4" onSubmit={handleSubmit}>
-            <div>
-              <input
-                type="email"
-                placeholder="Email Address or Phone number"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800 text-white"
-                id="username"
-                name="username"
-                value={input.username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800 text-white"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-3 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img
+            class="w-8 h-8 mr-2"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            alt="logo"
+          />
+          EmoTunes
+        </a>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form
+              class="space-y-4 md:space-y-6"
+              action="POST"
+              onSubmit={handleSubmit}
             >
-              Sign In
-            </button>
-          </form>
-          <div className="mt-4 text-center">
-            <span className="text-gray-400">New to EmoTunes? </span>
-            <b className="text-blue-600 cursor-pointer">
-            <Link to="/register" >Sign up Now!</Link></b>
-          </div>
-          <div className="mt-2 text-sm text-gray-400 text-center">
-            This page is protected by blah blah
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={inputs.email}
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                  onChange={handleChange}
+                  value={inputs.password}
+                />
+              </div>
+              <div class="flex items-center justify-between">
+                <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label
+                      for="remember"
+                      class="text-gray-500 dark:text-gray-300"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <a
+                  href="#"
+                  class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              <button
+                type="submit"
+                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Sign in
+              </button>
+              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?{" "}
+                <Link
+                  to="/register"
+                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
           </div>
         </div>
       </div>
-    </div>
-    </div>
+    </section>
   );
 }
--- src/files/register/Register.jsx
+++ src/files/register/Register.jsx
@@ -1,58 +1,167 @@
-import React, { useState, useRef } from 'react';
-import { Link } from "react-router-dom"
+import React, { useState, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { API_URL_LOGIN, API_URL_REGISTER } from "../../constants/config";
 
 export default function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
 
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const inputValue = type === "checkbox" ? checked : value;
 
-  const handleStart = () => {
-    setEmail(emailRef.current.value);
-  }
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: inputValue,
+    }));
+  };
 
-  const handleFinish = () => {
-    setPassword(passwordRef.current.value);
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/home");
 
+    // axios
+    //   .post(API_URL_LOGIN, inputs)
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+  };
   return (
-    <div className="bg-black min-h-screen flex justify-center items-center">
-      <div className="max-w-md w-full px-6 py-8 rounded-lg shadow-lg">
-        <div className="text-center mb-8">
-          <h1 className="text-3xl font-bold text-white">Get To know your emotions</h1>
-          <h2 className="text-xl font-semibold text-white">Ready to roll?</h2>
-          <p className="text-white">Enter your email to create or register</p>
-        </div>
-        {!email ? (
-          <div className="flex flex-row items-center">
-            <input
-              type="email"
-              placeholder="Email Address"
-              ref={emailRef}
-              className="w-full px-4 py-2 border border-gray-300  focus:outline-none focus:border-blue-500"
-            />
-            <button
-              className="bg-indigo-500 inline-block text-xs text-white py-2 px-4 hover:blue-900 focus:outline-none"
-              onClick={handleStart}
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img
+            class="w-8 h-8 mr-2"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            alt="logo"
+          />
+          EmoTunes
+        </a>
+        <p class="mb-6 text-lg text-center text-gray-600 dark:text-gray-300">
+          Want to listen to music? Create an account
+        </p>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Create an account
+            </h1>
+            <form
+              class="space-y-4 md:space-y-6"
+              action="#"
+              onSubmit={handleSubmit}
             >
-              Get Started
-            </button>
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={inputs.email}
+                />
+              </div>
+              <div>
+                <label
+                  for="username"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  username
+                </label>
+                <input
+                  type="username"
+                  name="username"
+                  id="username"
+                  placeholder="johndoe"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={inputs.username}
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={inputs.password}
+                />
+              </div>
+              <div class="flex items-start">
+                <div class="flex items-center h-5">
+                  <input
+                    id="terms"
+                    aria-describedby="terms"
+                    type="checkbox"
+                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                    required=""
+                  />
+                </div>
+                <div class="ml-3 text-sm">
+                  <label
+                    for="terms"
+                    class="font-light text-gray-500 dark:text-gray-300"
+                  >
+                    I accept the{" "}
+                    <a
+                      class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                      href="#"
+                    >
+                      Terms and Conditions
+                    </a>
+                  </label>
+                </div>
+              </div>
+              <button
+                type="submit"
+                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Create an account
+              </button>
+              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <Link
+                  to="/login"
+                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Login
+                </Link>
+              </p>
+            </form>
           </div>
-        ) : (
-          <form className="flex items-center">
-            <input
-              type="password"
-              placeholder="Enter Password"
-              ref={passwordRef}
-              className="w-full px-4 py-2 border border-gray-300  focus:outline-none focus:border-indigo-500 "
-            />
-            <Link to="/home" className="bg-indigo-500 inline-block text-white py-2 px-4  hover:bg-indigo-600 focus:outline-none">
-              Start
-            </Link>
-          </form>
-        )}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
